44c476294de7a315f652edc0014bbf9f760f9b98
lorenzo.cattarino <lorenzo.cattarino@ukhsa@gov.uk>
2025-03-13 11:15:41 +0000

    more changes following catch up with Jasmina

--- S3/scripts/data_challenge_dataset.R
+++ S3/scripts/data_challenge_dataset.R
@@ -3,6 +3,8 @@ library(purrr)
 library(magrittr)
 library(stringr)
 
+source("S3/scripts/data_challenge_dataset_functions.R")
+
 
 # define parameters -----------------------------------------------------------
 
@@ -32,25 +34,8 @@ feature_sets <- list.files(
   pattern = genes[1]) %>% 
   gsub("allflu_|_pt.*.rds", "", .)
 
-read_RDS <- function(x, focgene) {
-  
-  readRDS(x) %>% 
-    select(-any_of(c("segment", "cds_id", "enc", "GC_content"))) %>%
-    rename_with(~paste(., focgene, sep = "_"), -c(gid))
-  
-}
 
-read_featset <- function(focgene) {
-  
-  feature_sets_files <- list.files(
-    path = "S3/data/full/mlready", 
-    pattern = focgene, 
-    full.names = TRUE)
-  
-  feature_sets <- map(feature_sets_files, read_RDS, focgene = focgene) %>%
-    reduce(left_join, by = "gid")
-
-}
+set.seed(16354)
 
 
 # load data -------------------------------------------------------------------
@@ -66,7 +51,7 @@ holdout_cluster_grid <- list.files(
 
 subtypes <- unique(holdout_cluster_grid$subtype)
 
-labels <- read.csv(paste0(root, subtypes[1], "_", cluster_set, "_labels.csv")) %>% 
+labels <- read.csv(paste0(root, subtypes[8], "_", cluster_set, "_labels.csv")) %>% 
   select(cluster_rep, label) %>%
   # Rearrange factor levels for better compatibility with model functions
   mutate(label = factor(case_when(label == "zoon" ~ "hzoon", label == "nz" ~ "nz")) 
@@ -74,29 +59,29 @@ labels <- read.csv(paste0(root, subtypes[1], "_", cluster_set, "_labels.csv")) %
 
 allflu_wgs_ref <- read.csv("S3/data/full/allflu_wgs_ref.csv") %>%
   # Rearrange factor levels for better compatibility with model functions
-  mutate(label = factor(case_when(label == "zoon" ~ "hzoon", label == "nz" ~ "nz"))) 
+  mutate(label = factor(case_when(label == "zoon" ~ "hzoon", label == "nz" ~ "nz"))) %>%
+  select(gid, subtype, label, src)
 
 
 # processing ------------------------------------------------------------------
 
 
-# read and concatenate features for one gene only 
-# all_featsets <- read_featset(focgene = genes[1])
-
-# read and concatenate features for more genes 
-all_featsets_ls <- map(genes[c(1,3)], read_featset)
+# read and concatenate n_col random features for all genes 
+all_featsets_ls <- map(genes, read_featset, n_col = 10)
 all_featsets <- reduce(all_featsets_ls, left_join, by = "gid")
 
-# join to labels 
-all_featsets_labels <- left_join(labels, all_featsets, by = c("cluster_rep" = "gid"))
+# join features to labels (of the clustered dataset where one subtype was held out for test) 
+featsets_labels_cluster <- left_join(labels, all_featsets, by = c("cluster_rep" = "gid"))
 
-# test set
+# join features to labels (of the full dataset) 
+featsets_labels_full <- left_join(allflu_wgs_ref, all_featsets, by = "gid")
+
+# the clustered data set is to be tested on a separate set
 test_set <- left_join(
-  allflu_wgs_ref %>% 
-    filter(subtype == subtypes[1]) %>%
+  allflu_wgs_ref %>%
+    filter(subtype == subtypes[8]) %>%
     # Only consider zoonotic sequences for zoonotic holdouts
-    # filter(subtype %in% holdout_zoon & label == "hzoon"|subtype %in% holdout_nz) %>%  
-    select(gid, subtype, label, src),
+    filter(subtype %in% holdout_zoon & label == "hzoon"|subtype %in% holdout_nz),
   all_featsets,
   by = c("gid"))
 
@@ -105,22 +90,16 @@ test_set <- left_join(
 
 
 write.csv(
-  all_featsets_labels, 
-  file = sprintf("train_eval_set_%s_%s.csv", genes[1], genes[3]), 
-  row.names = FALSE) 
+  featsets_labels_cluster,
+  file = "zoonosis_dataset_train_eval.csv",
+  row.names = FALSE)
 
 write.csv(
   test_set,
-  file = sprintf("test_set_%s_%s.csv", genes[1], genes[3]), 
+  file = "zoonosis_dataset_test.csv",
   row.names = FALSE)
 
-
-# option 1 provide all sequences and let them do the split (and potentially clustering?)
-# 19531 records x 13303 features 
-# 2.5G csv file 
-
-# option 2 provide only the cluster representative sequences and associated test set 
-# 2123 records x 13303 features
-# 250K file
-
-# either ways I am only providing features for two genes only (too many features otherwise)
+write.csv(
+  featsets_labels_full,
+  file = "zoonosis_dataset_full.csv", 
+  row.names = FALSE)
