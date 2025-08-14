906b3f206d3d73843678c9e7cff2a350801f035d
lorenzo.cattarino <lorenzo.cattarino@ukhsa@gov.uk>
2025-08-14 15:06:55 +0100

    Revert "added changes"
    This reverts commit 6f3f196097770e78a04e42456a9c1056cbe9eb71, reversing
    changes made to 4620b91ce1c1251d0a5eaa0b0f21474ff69c7be6.

--- S3/scripts/create_training_fold_indices.R
+++ S3/scripts/create_training_fold_indices.R
@@ -23,40 +23,26 @@ cluster_set <- "70_7"
 
 fold_indices_list <- list()
 
-# #############################################################
-# # Extract training folds from previously run model and save #
-# #############################################################
-# 
-# model_list <- list.files(path = "S3/analysis", pattern = ".rds", recursive = TRUE, full.names = TRUE) %>%
-#   .[grepl(cluster_set, .)] %>%
-#   .[grepl("results_14_02_24", .)] %>%
-#   .[1] %>%
-#   readRDS
-# 
-<<<<<<< HEAD
-# for(i in 1:length(unique(holdout_cluster_grid$subtype))){
-#   
-#   fold_indices_list[[i]] <- model_list[[i]]$control$index
-#   
-# }
+#############################################################
+# Extract training folds from previously run model and save #
+#############################################################
+
+model_list <- list.files(path = "S3/analysis", pattern = ".rds", recursive = TRUE, full.names = TRUE) %>%
+  .[grepl(cluster_set, .)] %>%
+  .[grepl("results_14_02_24", .)] %>%
+  .[1] %>%
+  readRDS
+
+for(i in 1:length(unique(holdout_cluster_grid$subtype))){
   
-#############################################
-# Generate training folds a priori and save #
-#############################################
-
-fold_fun <- function(subtype){
-
-  labels <- read.csv(paste0("S3/data/full/holdout_clusters/ex_", subtype, "_", cluster_set, "_labels.csv")) %>%
-    select(cluster_rep, label) %>%
-    mutate(label = factor(case_when(label == "zoon" ~ "hzoon", label == "nz" ~ "nz")) # Rearrange factor levels for better compatibility with model functions
-    )
-
-  # Create folds for 5-fold cross-validation
-  set.seed(1657)
-  createMultiFolds(labels$label, k = 5, times = 1) %>%
-    return()
+  fold_indices_list[[i]] <- model_list[[i]]$control$index
+  
 }
-=======
+
+# #############################################
+# # Generate training folds a priori and save #
+# #############################################
+# 
 # fold_fun <- function(subtype){
 # 
 #   labels <- read.csv(paste0("S3/data/full/holdout_clusters/ex_", subtype, "_", cluster_set, "_labels.csv")) %>%
@@ -71,8 +57,6 @@ fold_fun <- function(subtype){
 # }
 # 
 # fold_indices_list <- Map(f = fold_fun, subtype = unique(holdout_cluster_grid$subtype))
->>>>>>> 4620b91ce1c1251d0a5eaa0b0f21474ff69c7be6
 
-fold_indices_list <- Map(f = fold_fun, subtype = unique(holdout_cluster_grid$subtype))
 names(fold_indices_list) <- unique(holdout_cluster_grid$subtype)
 fold_indices_list %>% saveRDS("S3/data/fold_indices_list.rds")
